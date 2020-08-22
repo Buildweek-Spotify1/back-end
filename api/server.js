37e6332e0ec44d9ff26db147e710b28ed9c5dae7
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-08-21 22:44:49 -0400

    added cors package

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
 const authRouter = require("../auth/auth-router");
 
 server.use(express.json());
+server.use(cors());
 
 server.use("/api/auth", authRouter);
 
