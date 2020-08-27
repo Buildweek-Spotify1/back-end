566eb19389af85bc88d0665830d1a3f8ece034b6
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-08-26 22:25:56 -0400

    found minor error

--- songs/songs-router.js
+++ songs/songs-router.js
@@ -2,30 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Axios = require("axios");
 const qs = require("qs");
+require("dotenv").config();
 
-router.get("/search", (req,res)=>{
+router.get("/search", (req, res) => {
     Axios.post('https://accounts.spotify.com/api/token',
-      qs.stringify({
-        grant_type: 'client_credentials'
-      }),
-      {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        auth: {
-          username: process.env.CLIENT_ID || "4774db1a6cc749d2b07ce2d23707c3df",
-          password: process.env.CLIENT_SECRET || "2b7d1098a7774a369c2a9f9d7d683ee1"
+        qs.stringify({
+            grant_type: 'client_credentials'
+        }),
+        {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: process.env.CLIENT_ID || "4774db1a6cc749d2b07ce2d23707c3df",
+                password: process.env.CLIENT_SECRET || "2b7d1098a7774a369c2a9f9d7d683ee1"
+            }
         }
-      }
     )
-      .then(res => {
-        res.status(200).json({spotifyToken: res.data.access_token})
-        console.log(res)
-      })
-      .catch(err => {
-        console.log(err)
-      })
+        .then(response => {
+            res.status(200).json({ spotifyToken: response.data.access_token })
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err.response.data.error)
+        })
 })
 
 module.exports = router;
